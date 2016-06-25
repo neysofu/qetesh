d1e948d97dbeefa620334a87db7abb924abc1dac
neysofu <filippocosta.italy@gmail.com>
2016-06-25 14:36:17 +0200

    Fixed typo

--- qetesh-src/harvest/settings.py
+++ qetesh-src/harvest/settings.py
@@ -9,7 +9,7 @@ CONCURRENT_REQUESTS = 4
 COOKIES_ENABLED = False
 
 _src = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-_target = os.path.join(os.path.dirname(__src__), 'target')
+_target = os.path.join(os.path.dirname(_src), 'target')
 JOBDIR = os.path.join(_target, 'resume')
 FEED_URI = os.path.join(_target, 'crawled')
 FEED_FORMAT = 'csv'
