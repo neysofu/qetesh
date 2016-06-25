6dd08a41e83c52b495c116d28ae4dc61a2262753
neysofu <filippocosta.italy@gmail.com>
2016-06-25 14:07:26 +0200

    .gitignore, requirements.txt, rename src -> qetesh-src

---
+++ qetesh-src/harvest/settings.py
@@ -0,0 +1,15 @@
+import os
+
+BOT_NAME = 'harvest'
+SPIDER_MODULES = ['harvest.spiders']
+NEWSPIDER_MODULE = 'harvest.spiders'
+
+DOWNLOAD_DELAY = 3
+CONCURRENT_REQUESTS = 4
+COOKIES_ENABLED = False
+
+_src = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+_target = os.path.join(os.path.dirname(__src__), 'target')
+JOBDIR = os.path.join(_target, 'resume')
+FEED_URI = os.path.join(_target, 'crawled')
+FEED_FORMAT = 'csv'
